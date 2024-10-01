56b6a30f780a5e552ea45de06ee7e889a8fab90a
chinapat <chinapat.r@ku.th>
2024-10-01 16:29:10 +0700

    add json display

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const connectDB = require("./db.js");
 
 const app = express ();
+app.use(express.json)
 app.use(cors());
 const PORT = process.env.PORT || 3000;
 connectDB.connectDB();
